fc08bbbb7894f4e2b5b1bb17d279ae4b3be54696
Tomasz Laboj <tomasz.laboj@ocado.com>
2024-09-05 12:04:07 +0100

    self invoking function description

--- src/self-invoking-fn.js
+++ src/self-invoking-fn.js
@@ -3,6 +3,7 @@
   console.log("some text");
 })();
 
+// ! is not need for IIFE (Immedietaly Invoked Function Expression) but it was used in the early days of js , ! is logical NOT so it negates the return value
 !(function returnFalse() {
   return false;
 })();
